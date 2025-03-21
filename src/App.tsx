d1ea75699ed087da180a665ed094512d2a33350a
raffe <rafe2004@gmail.com>
2025-03-20 22:37:40 -0600

    fix #root style add dark theme to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,51 @@ import SessionProvider from "./context/ctx";
 import AuthGuard from "./guards/auth.guard";
 import { PrivateRoutes, PublicRoutes } from "./models";
 import { RoutesWithNotFound } from "./utilities";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-const LoginPage = lazy(() => import('./pages/Login/Login'));
-const Private = lazy(() => import('./pages/Private/Private'));
+const LoginPage = lazy(() => import("./pages/Login/Login"));
+const Private = lazy(() => import("./pages/Private/Private"));
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
     <SessionProvider>
-      <div>
-        <Suspense
-          fallback={
-            <>
-              <h3>Loading...</h3>
-            </>
-          }
-        >
-          <BrowserRouter>
-            <RoutesWithNotFound>
-              <Route
-                path="/"
-                element={<Navigate to={PrivateRoutes.PRIVATE} />}
-              ></Route>
-              <Route path={PublicRoutes.LOGIN} element={<LoginPage />}></Route>
-              <Route element={<AuthGuard />}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline enableColorScheme />
+        <div className="main-layout">
+          <Suspense
+            fallback={
+              <>
+                <h3>Loading...</h3>
+              </>
+            }
+          >
+            <BrowserRouter>
+              <RoutesWithNotFound>
                 <Route
-                  path={`${PrivateRoutes.PRIVATE}/*`}
-                  element={<Private />}
-                />
-              </Route>
-            </RoutesWithNotFound>
-          </BrowserRouter>
-        </Suspense>
-      </div>
+                  path="/"
+                  element={<Navigate to={PrivateRoutes.PRIVATE} />}
+                ></Route>
+                <Route
+                  path={PublicRoutes.LOGIN}
+                  element={<LoginPage />}
+                ></Route>
+                <Route element={<AuthGuard />}>
+                  <Route
+                    path={`${PrivateRoutes.PRIVATE}/*`}
+                    element={<Private />}
+                  />
+                </Route>
+              </RoutesWithNotFound>
+            </BrowserRouter>
+          </Suspense>
+        </div>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
