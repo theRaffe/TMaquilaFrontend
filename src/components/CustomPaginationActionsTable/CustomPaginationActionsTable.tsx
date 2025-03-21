1b91379338140298b9aba83bf91ec89fcb477be2
raffe <rafe2004@gmail.com>
2025-03-21 12:48:41 -0600

    add feature filter of vendor name in table

--- src/components/CustomPaginationActionsTable/CustomPaginationActionsTable.tsx
+++ src/components/CustomPaginationActionsTable/CustomPaginationActionsTable.tsx
@@ -118,30 +118,11 @@ function TablePaginationActions(props: TablePaginationActionsProps) {
   );
 }
 
-// function createData(name: string, calories: number, fat: number) {
-//   return { name, calories, fat };
-// }
-
-// const rows = [
-//   createData('Cupcake', 305, 3.7),
-//   createData('Donut', 452, 25.0),
-//   createData('Eclair', 262, 16.0),
-//   createData('Frozen yoghurt', 159, 6.0),
-//   createData('Gingerbread', 356, 16.0),
-//   createData('Honeycomb', 408, 3.2),
-//   createData('Ice cream sandwich', 237, 9.0),
-//   createData('Jelly Bean', 375, 0.0),
-//   createData('KitKat', 518, 26.0),
-//   createData('Lollipop', 392, 0.2),
-//   createData('Marshmallow', 318, 0),
-//   createData('Nougat', 360, 19.0),
-//   createData('Oreo', 437, 18.0),
-// ].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
 interface TableProps {
   rows: any[];
   columnData: ColumnConfig[];
   totalPages: number;
+  filterFn: { fn: (items:any[]) => any[] };
   changePageEvent: (pageIndex: number, pageSize: number) => void;
 }
 
@@ -150,6 +131,7 @@ export default function CustomPaginationActionsTable({
   columnData,
   totalPages,
   changePageEvent,
+  filterFn,
 }: TableProps) {
   const [page, setPage] = React.useState(1);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -174,6 +156,8 @@ export default function CustomPaginationActionsTable({
     setPage(0);
   };
 
+  const filteredRows = filterFn.fn(rows);
+
   useEffect(() => {
     console.log('exec init changePageEvent');
     changePageEvent(page, rowsPerPage);
@@ -197,10 +181,8 @@ export default function CustomPaginationActionsTable({
           </TableRow>
         </TableHead>
         <TableBody>
-          {(rowsPerPage > 0
-            ? rows
-            : rows
-          ).map((row) => (
+          {filteredRows
+          .map((row) => (
             <TableRow key={row["id"]}>
               {columnData.map((column) => (
                 <TableCell
--- src/pages/Private/Dashboard/Dashboard.tsx
+++ src/pages/Private/Dashboard/Dashboard.tsx
@@ -2,6 +2,8 @@ import { CustomPaginationTable } from "@/components/CustomPaginationActionsTable
 import { useEffect, useState } from "react";
 import * as loadsService from "@/services/LoadsService";
 import { ColumnConfig } from "@/components/CustomPaginationActionsTable/Column.model";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function Dashboard() {
   interface PaginationState {
@@ -14,6 +16,11 @@ function Dashboard() {
   });
   const [dataRows, setDataRows] = useState<any[]>([]);
   const [totalPages, setTotalPages] = useState(0);
+  const [filterFn, setFilterFn] = useState({
+    fn: (items) => {
+      return items;
+    },
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,8 +30,7 @@ function Dashboard() {
       );
       setDataRows(data.loads);
       setTotalPages(data.totalPages);
-      console.log({dataRows: data.loads});
-
+      console.log({ dataRows: data.loads });
     };
 
     if (paginationState.pageIndex) {
@@ -35,6 +41,25 @@ function Dashboard() {
   const onChangePageEvent = (pageIndex: number, pageSize: number) => {
     setPaginationState({ pageIndex, pageSize });
   };
+
+  const handleFilterChange = (e) => {
+    const target = e.target;
+    setFilterFn({
+        fn: items => {
+            if (target.value == "")
+                return items;
+            else {
+              //console.log({value: target.value, items});
+              const valueSearch = target.value.toLowerCase();
+              return items.filter(item => {
+                console.log({item, value: valueSearch});
+                return item.vendorName.toLowerCase().includes(valueSearch);
+              });
+            }
+                
+        }
+    })
+}
 
   const columns: ColumnConfig[] = [
     {
@@ -65,12 +90,22 @@ function Dashboard() {
   ];
 
   return (
-    <CustomPaginationTable
-      rows={dataRows}
-      columnData={columns}
-      totalPages={totalPages}
-      changePageEvent={onChangePageEvent}
-    />
+    <Box  sx={{ marginBottom: 2, gap: 2, display: 'flex', flexFlow: 'column', padding: 8 }}>
+      <TextField
+        label="Filter by Vendor"
+        variant="outlined"
+        onChange={handleFilterChange}
+        fullWidth
+        sx={{ maxWidth: 400 }}
+      />
+      <CustomPaginationTable
+        rows={dataRows}
+        columnData={columns}
+        totalPages={totalPages}
+        filterFn={filterFn}
+        changePageEvent={onChangePageEvent}
+      />
+    </Box>
   );
 }
 
